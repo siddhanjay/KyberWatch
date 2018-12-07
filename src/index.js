fb0a0b6a3ddb720ffbe7c8d96f5eb3f3dad18632
Robin Thomas <robinthomas2591@gmail.com>
2018-12-08 01:47:29 +0800

    create get end point to get token trade details

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 80;
 
 // Return a list of currencies supported by Kyber.
-app.get(config.app.path + '/kyber/currencies', async (req, res) => {
+app.get(config.app.path + '/currencies', async (req, res) => {
   let currencies = [];
   try {
     currencies = await Kyber.getSupportedCurrencies();
@@ -14,6 +14,38 @@ app.get(config.app.path + '/kyber/currencies', async (req, res) => {
   }
 
   res.status(200).send({status: "ok", results: currencies});
+});
+
+app.get(config.app.path + '/currencies/:token', async (req, res) => {
+  // Check if token is supported.
+  let token = null;
+  try {
+    token = req.params.token;
+    const supportedCurrencies = await Kyber.getSupportedCurrencies();
+    if (supportedCurrencies.indexOf(token) === -1) {
+      throw new Error('Token is not supported by Kyber');
+    }
+  } catch (err) {
+    res.status(400).send({status: "error", error: err.message});
+    return;
+  }
+
+  // Check for missing parameters.
+  if (typeof req.query.start === 'undefined') {
+    res.status(400).send({status: "error", error: "missing start timestamp"});
+    return;
+  }
+  if (typeof req.query.stop === 'undefined') {
+    res.status(400).send({status: "error", error: "missing stop timestamp"});
+    return;
+  }
+
+  // Validate the timestamps.
+
+  // Convert the timestamp to block numbers.
+  const blocks = Utils.getBlocksBetweenTimestamps(req.query.start, req.query.stop);
+
+  res.status(200).send({status: "ok", message: "Token is supported by Kyber"});
 });
 
 app.listen(port, () => console.log(`app listening on ${port}`));
---
+++ src/utils.js
@@ -0,0 +1,85 @@
+const config = require('./common/config.json');
+
+const Web3 = require('web3');
+let web3 = new Web3(new Web3.providers.HttpProvider(config.infura.mainnet.host));
+
+const getLastBlockBeforeTimestamp = async (currentBlock, timestamp) => {
+  // Find the block closest to stop.
+  // Assuming 1 block per 15 seconds.
+  let lastBlockTimestamp = currentBlock.timestamp;
+  let diffBlocks = Math.floor((lastBlockTimestamp - timestamp) / 15);
+  let lastBlock = currentBlock.block - diffBlocks;
+
+  do {
+    lastBlockTimestamp = (await web3.eth.getBlock(lastBlock)).timestamp;
+    if (lastBlockTimestamp > timestamp) {
+      diffBlocks = Math.floor((lastBlockTimestamp - timestamp) / 15);
+      diffBlocks = diffBlocks == 0 ? 1 : diffBlocks;
+      lastBlock -= diffBlocks;
+    } else {
+      const nextBlockTimestamp = (await web3.eth.getBlock(lastBlock + 1)).timestamp;
+      if (nextBlockTimestamp >= timestamp) {
+        lastBlock = (nextBlockTimestamp === timestamp) ? lastBlock + 1 : lastBlock;
+        break;
+      }
+      ++lastBlock;
+    }
+  } while (true);
+
+  return lastBlock;
+}
+
+const getFirstBlockAfterTimestamp = async (lastBlock, timestamp) => {
+  // Find the block closest to start.
+  // Assuming 1 block per 15 seconds.
+  let startBlockTimestamp = lastBlock.timestamp;
+  diffBlocks = Math.floor((startBlockTimestamp - timestamp) / 15);
+  let startBlock = lastBlock.block - diffBlocks;
+
+  do {
+    if (startBlock == 0) {
+      break;
+    }
+    startBlockTimestamp = (await web3.eth.getBlock(startBlock)).timestamp;
+    if (startBlockTimestamp < timestamp) {
+      ++startBlock;
+    } else {
+      const prevBlockTimestamp = (await web3.eth.getBlock(startBlock - 1)).timestamp;
+      if (prevBlockTimestamp <= timestamp) {
+        startBlock = (prevBlockTimestamp === timestamp) ? (startBlock - 1) : startBlock;
+        break;
+      }
+
+      diffBlocks = Math.floor((startBlockTimestamp - timestamp) / 15);
+      if (diffBlocks === 0) {
+        break;
+      }
+      startBlock -= diffBlocks;
+    }
+  } while (true);
+
+  return startBlock;
+}
+
+const Utils = {
+  getBlocksBetweenTimestamps: async (start, stop) => {
+    const currentBlock = await web3.eth.getBlockNumber();
+    const currentBlockTimestamp = (await web3.eth.getBlock(currentBlock)).timestamp;
+
+    // Sanitize range if out of bound.
+    start = (start < 0) ? 0 : start;
+    stop = (stop > currentBlockTimestamp) ? currentBlockTimestamp : stop;
+
+    const endBlock = await getLastBlockBeforeTimestamp(
+      {block: currentBlock, timestamp: currentBlockTimestamp}, start);
+    const startBlock = await getFirstBlockAfterTimestamp(
+      {block: endBlock, timestamp: (await web3.eth.getBlock(endBlock)).timestamp}, stop);
+
+    return {
+      startBlock: startBlock,
+      endBlock: endBlock,
+    };
+  },
+};
+
+module.exports = Utils;
