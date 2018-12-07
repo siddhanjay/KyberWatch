7835a16f803302bc72460237d819b9b0536ea699
Robin Thomas <robinthomas2591@gmail.com>
2018-12-08 00:01:36 +0800

    make an expressjs node app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const config = require('../common/config.json');
+
+const app = express();
+const port = 4000;
+
+// Return a list of currencies supported by Kyber.
+app.get(config.app.path + '/kyber/currencies', (req, res) => {
+  res.status(200).send({status: "ok", results: []});
+});
+
+app.listen(port, () => console.log(`app listening on ${port}`));
